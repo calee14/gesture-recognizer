9951b830b31bf330b5b7ba32b97dabe03ae47217
calee14 <caplee1000@gmail.com>
2018-11-16 10:48:18 -0800

    setting up package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="gesture-recognizer",
+	version="0.0.1",
+	author="capjedi",
+	author_email="calee14@s.sfusd.edu",
+	description="A basic gesture recognizer module for a hackathon.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/calee14/gesture-recognizer",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
