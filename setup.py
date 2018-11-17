1f3aec6e6ce3c34472498fc68f54fd56870184d1
calee14 <caplee1000@gmail.com>
2018-11-17 12:27:22 -0800

    changine version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="gesture-recognizer",
-	version="0.2.0",
+	version="0.2.1",
 	author="capjedi",
 	author_email="calee14@s.sfusd.edu",
 	license='MIT',
