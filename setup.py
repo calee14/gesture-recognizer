230099134efaddd2c33d25dff312cbbdcb171788
calee14 <caplee1000@gmail.com>
2018-11-18 02:38:55 -0800

    adding to pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="gesture-recognizer",
-	version="0.3.9",
+	version="0.3.10",
 	author="capjedi",
 	author_email="calee14@s.sfusd.edu",
 	license='MIT',
